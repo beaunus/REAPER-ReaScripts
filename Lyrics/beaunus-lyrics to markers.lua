7d38b54632403c512ef94622883a144fba78255a
Beau Dobbin <beau@beaunus.com>
2017-01-01 14:36:37 +0900

    added lyrics to markers

---
+++ Lyrics/beaunus-lyrics to markers.lua
@@ -0,0 +1,11 @@
+--[[
+ * Version: 1.0
+ * Main: False
+--]]
+
+---------------------------------------------------
+-- A function to get the average RMS from a take --
+-- + an example (at the end)                     --
+---------------------------------------------------
+
+reaper.ShowMessageBox("Hello World", "Script code here", 0)
