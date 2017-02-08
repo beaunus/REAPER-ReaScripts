b976ad8d6bac3ffad2c621558f1dc8a2b3cdbf97
Beau Dobbin <beau@beaunus.com>
2017-02-08 11:48:29 +0900

    added comments and segregated add_clip()

--- Clip Splicer/beaunus_Import reaper-clip-splicer JSON.py
+++ Clip Splicer/beaunus_Import reaper-clip-splicer JSON.py
@@ -76,13 +76,13 @@ def add_pause(component, cur_position, track_objects, pause_lengths):
         since it may have been modified.
       new_item: The new RPR_MediaItem that represents the newly added component.
   """
-  # Get the RPR_MediaTrack for pauses
+  # Get the RPR_MediaTrack for pauses.
   (pause_track, track_objects) = get_track("_PAUSE", track_objects)
 
   # Add a new item to the pause_track.
   new_item = RPR_AddMediaItemToTrack(pause_track)
 
-  # Determine the length of the pause
+  # Determine the length of the pause.
   if (pause_lengths is not None and component in pause_lengths):
     RPR_SetMediaItemInfo_Value(new_item, "D_LENGTH", pause_lengths[component])
   else:
@@ -98,6 +98,72 @@ def add_pause(component, cur_position, track_objects, pause_lengths):
 
   # Increment the cursor_position and return
   cur_position += RPR_GetMediaItemInfo_Value(new_item, "D_LENGTH")
+  return (cur_position, track_objects, new_item)
+
+def add_repeat(component, cur_position, track_objects, prev_item):
+  """Adds a repeat specified by the given component to the session.
+
+  Args:
+    component: The JSON representation of the component.
+    cur_position: The starting position of the component.
+    track_objects: The array of REAPER track objects in which to add components.
+    prev_item: The previous RPR_MediaItem.
+
+  Returns:
+    A tuple containing:
+      cur_position: The new cursor position, after adding the component.
+      track_objects: The array of RPR_MediaTracks,
+        since it may have been modified.
+      new_item: The new RPR_MediaItem that represents the newly added component.
+  """
+  # Get the RPR_MediaTrack for repeats.
+  (repeat_track, track_objects) = get_track("_REPEAT", track_objects)
+
+  # Add a new item to the repeat_track.
+  new_item = RPR_AddMediaItemToTrack(repeat_track)
+
+  # Determine the previous item's length and name.
+  prev_item_length = RPR_GetMediaItemInfo_Value(prev_item, "D_LENGTH")
+  prev_item_name = \
+  RPR_GetSetMediaItemTakeInfo_String(RPR_GetActiveTake(prev_item), \
+                                     "P_NAME", None, False)[3]
+  # Set the repeat item's attributes and add a new take.
+  RPR_SetMediaItemInfo_Value(new_item, "D_LENGTH", prev_item_length)
+  RPR_SetMediaItemInfo_Value(new_item, "D_POSITION", cur_position)
+  RPR_SetMediaItemInfo_Value(new_item, "B_MUTE", True)
+  RPR_AddTakeToMediaItem(new_item)
+
+  # Increment the cur_position.
+  cur_position += RPR_GetMediaItemInfo_Value(new_item, "D_LENGTH")
+
+  # Get the newly created take and name it accordingly.
+  this_take = RPR_GetActiveTake(new_item)
+  RPR_GetSetMediaItemTakeInfo_String(this_take, "P_NAME", prev_item_name, True)
+
+  return (cur_position, track_objects, new_item)
+
+def add_clip(component, cur_position, track_objects, prev_item):
+  # Determine the performer of the clip.
+  performer = get_performer(component)
+
+  # Get the track that the clip should be inserted into.
+  (track, track_objects) = get_track(performer, track_objects)
+  new_item = RPR_AddMediaItemToTrack(track)
+
+  # Set the length and position of the new RPR_MediaItem
+  RPR_SetMediaItemInfo_Value(new_item, "D_LENGTH", 1)
+  RPR_SetMediaItemInfo_Value(new_item, "D_POSITION", cur_position)
+
+  # Add a RPR_Take to the new media item.
+  RPR_AddTakeToMediaItem(new_item)
+
+  # Increment the cur_position.
+  cur_position += RPR_GetMediaItemInfo_Value(new_item, "D_LENGTH")
+
+  # Name the take according to the component.
+  this_take = RPR_GetActiveTake(new_item)
+  RPR_GetSetMediaItemTakeInfo_String(this_take, "P_NAME", component, True)
+
   return (cur_position, track_objects, new_item)
 
 def add_component(component, cur_position, track_objects, pause_lengths, \
@@ -123,28 +189,15 @@ def add_component(component, cur_position, track_objects, pause_lengths, \
   if component.startswith("_PAUSE"):
     (cur_position, track_objects, new_item) = \
     add_pause(component, cur_position, track_objects, pause_lengths)
+  # If this component is a _REPEAT, add a muted copy of the previous item
+  # to represent it.
   elif component == "_REPEAT_PREVIOUS_WORD":
-    (repeat_track, track_objects) = get_track("_REPEAT", track_objects)
-    new_item = RPR_AddMediaItemToTrack(repeat_track)
-    prev_item_length = RPR_GetMediaItemInfo_Value(prev_item, "D_LENGTH")
-    prev_item_name = RPR_GetSetMediaItemTakeInfo_String(RPR_GetActiveTake(prev_item), "P_NAME", None, False)[3]
-    RPR_SetMediaItemInfo_Value(new_item, "D_LENGTH", prev_item_length)
-    RPR_SetMediaItemInfo_Value(new_item, "D_POSITION", cur_position)
-    RPR_SetMediaItemInfo_Value(new_item, "B_MUTE", True)
-    RPR_AddTakeToMediaItem(new_item)
-    cur_position += RPR_GetMediaItemInfo_Value(new_item, "D_LENGTH")
-    this_take = RPR_GetActiveTake(new_item)
-    RPR_GetSetMediaItemTakeInfo_String(this_take, "P_NAME", prev_item_name, True)
+    (cur_position, track_objects, new_item) = \
+    add_repeat(component, cur_position, track_objects, prev_item)
+  # Otherwise, add the performed clip to the session.
   else:
-    performer = get_performer(component)
-    (track, track_objects) = get_track(performer, track_objects)
-    new_item = RPR_AddMediaItemToTrack(track)
-    RPR_SetMediaItemInfo_Value(new_item, "D_LENGTH", 1)
-    RPR_SetMediaItemInfo_Value(new_item, "D_POSITION", cur_position)
-    RPR_AddTakeToMediaItem(new_item)
-    cur_position += RPR_GetMediaItemInfo_Value(new_item, "D_LENGTH")
-    this_take = RPR_GetActiveTake(new_item)
-    RPR_GetSetMediaItemTakeInfo_String(this_take, "P_NAME", component, True)
+    (cur_position, track_objects, new_item) = \
+    add_clip(component, cur_position, track_objects, prev_item)
   return (cur_position, track_objects, new_item)
 
 if __name__ == "__main__":
